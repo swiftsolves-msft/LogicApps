9f2a4f4a268e2b9a3fdede37576d08a968d7657a
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2023-04-05 22:25:30 -0400

    Add files via upload

---
+++ XDRAlertIsolate-MDEDevice/AddApiPermissions.ps1
@@ -0,0 +1,44 @@
+﻿# This script grants the necessary Microsoft Graph API permissions to the Service Principal.
+# Before running, change the $TenantID (line5) to your AAD Tenant ID and the $DisplayNameofMSI (line6) to the name of your Logic App
+# This script requires the AzureAD Powershell Module,  Install-Module AzureAD
+
+$TenantID=""  #AAD Tenant Id
+$DisplayNameOfMSI="XDRAlertIsolate-MDEDevice" # Name of the managed identity
+
+Connect-AzureAD -TenantId $TenantID
+
+$MI = (Get-AzureADServicePrincipal -Filter "displayName eq '$DisplayNameOfMSI'")
+
+Start-Sleep -Seconds 5
+
+
+#Defender for Endpoint API - Machine.Isolate
+$GraphAppId = "fc780465-2017-40d4-a0c5-307022471b92"
+$PermissionName1 = "Machine.Isolate"
+
+$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
+$AppRole1 = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName1 -and $_.AllowedMemberTypes -contains "Application"}
+New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId `
+-ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole1.Id
+
+Start-Sleep -Seconds 5
+
+#Defender for Endpoint API - AdvancedQuery.Read.All
+$GraphAppId = "fc780465-2017-40d4-a0c5-307022471b92"
+$PermissionName2 = "AdvancedQuery.Read.All"
+
+$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
+$AppRole1 = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName2 -and $_.AllowedMemberTypes -contains "Application"}
+New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId `
+-ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole1.Id
+
+Start-Sleep -Seconds 5
+
+#Microsoft Graph API - Policy.Read.All
+$GraphAppId = "00000003-0000-0000-c000-000000000000"
+$PermissionName2 = "Policy.Read.All"
+
+$GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
+$AppRole1 = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName2 -and $_.AllowedMemberTypes -contains "Application"}
+New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId `
+-ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole1.Id
---
+++ XDRAlertIsolate-MDEDevice/readme.md
@@ -0,0 +1,22 @@
+# XDRAlertIsolate-MDEDevice
+
+author: Nathan Swift
+
+The following Logic App is designed to poll Security Graph frequently 1 minute for High Severity Alerts. Upon a High Alert trigger and lookup DeviceId and Isolate Device
+
+## Quick Deployment
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fswiftsolves-msft%2FLogicApps%2Fmaster%2FXDRAlertIsolate-MDEDevice%2Fazuredeploy.json" target="_blank">
+    <img src="https://aka.ms/deploytoazurebutton"/>
+</a>
+<a href="https://portal.azure.us/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fswiftsolves-msft%2FLogicApps%2Fmaster%2FXDRAlertIsolate-MDEDevice%2Fazuredeploy.json" target="_blank">
+<img src="https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/1-CONTRIBUTION-GUIDE/images/deploytoazuregov.png"/>
+</a>
+
+## Prerequisites
+
+- None
+
+## After Deployment
+
+- Grant the Logic App Managed Identity access to the Microsoft Graph SecurityEvents.Read.All & Machine.Isolate & AdvancedQuery.Read.All which can be done with the included PowerShell script [AddApiPermissions.ps1](https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/Solutions/Standalone/Playbooks/Add-IP-Entity-To-Named-Location/AddApiPermissions.ps1)
